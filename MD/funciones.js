74c31aeaaa6383abf67e93a9b108a1fa04b63c9f
Mauricio Martinez <MauricioAntonioMartinez@outlook.com>
2020-03-03 13:57:56 -0600

    Add files via upload

---
+++ MD/funciones.js
@@ -0,0 +1,11 @@
+function dark() {
+    document.body.style.background = "black";
+    document.body.style.color = "white";
+    document.getElementsByTagName("div").style.background = "black";
+    document.getElementsByClassName("caja1").style.background = "black";
+
+  }
+  function normal() {
+    document.body.style.background = "white";
+    document.body.style.color = "black";
+  }
